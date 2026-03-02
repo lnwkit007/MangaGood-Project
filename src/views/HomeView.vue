251eba392c3d010a98d374c53f88bdda4b98b10f
lnwkit007 <164681756+lnwkit007@users.noreply.github.com>
2026-03-02 19:22:11 +0700

    Update Loadding Page.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,6 +5,7 @@ import axios from "axios";
 // import components
 import MangaGrid from "../components/MangaGrid.vue";
 import RecommendedManga from "../components/RecommendedManga.vue";
+import Loadding from "../components/Loadding.vue";
 
 const mangas = ref([]);
 const loading = ref(true);
@@ -23,8 +24,24 @@ onMounted(async () => {
 </script>
 
 <template>
-  <section class="min-h-screen pt-8 pb-13 sm:pt-10 sm:pb-15">
-    <div class="mx-auto grid max-w-330 gap-4 px-4 lg:grid-flow-col lg:gap-2">
+  <section
+    v-if="error"
+    class="flex min-h-screen items-center justify-center border"
+  >
+    <p class="rounded-sm bg-red-600 px-5 py-3 font-semibold text-white">
+      {{ error }}
+    </p>
+  </section>
+
+  <section v-else-if="!error" class="min-h-screen pt-8 pb-13 sm:pt-10 sm:pb-15">
+    <div v-if="loading" class="flex h-screen items-center justify-center">
+      <Loadding />
+    </div>
+
+    <div
+      v-else-if="!loading"
+      class="mx-auto grid max-w-330 gap-4 px-4 lg:grid-flow-col lg:gap-2"
+    >
       <div class="col-span-9 flex flex-col gap-4">
         <MangaGrid :mangas="mangas" />
         <MangaGrid :mangas="mangas" />
